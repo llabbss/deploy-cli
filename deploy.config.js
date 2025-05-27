a3d1c6d64c18c758a9c4ee6861a74b2f7386b957
liaocy <lcy@sh-senyu.com>
2025-05-27 11:40:17 +0800

    feat: rename filename

--- deploy.config.js
+++ deploy.config.js
@@ -2,8 +2,8 @@
  * @Author: Oliver
  * @Date: 2025-05-27 09:57:24
  * @LastEditors: Oliver
- * @LastEditTime: 2025-05-27 10:57:35
- * @FilePath: /cli/vite-deploy.config.js
+ * @LastEditTime: 2025-05-27 11:40:07
+ * @FilePath: /cli/deploy.config.js
  */
 export default (() => ({
   server: "",
